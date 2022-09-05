a885ca4bc39896a551435140e82cebc5096f365a
Brian <brian.nejati@utoronto.ca>
2022-09-04 21:12:32 -0400

    #91 update

--- python/91-Decode-ways.py
+++ python/91-Decode-ways.py
@@ -1,34 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # Memoization
-        dp = {len(s): 1}
-
-        def dfs(i):
-            if i in dp:
-                return dp[i]
-            if s[i] == "0":
-                return 0
-
-            res = dfs(i + 1)
-            if i + 1 < len(s) and (
-                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
-            ):
-                res += dfs(i + 2)
-            dp[i] = res
-            return res
-
-        return dfs(0)
-
-        # Dynamic Programming
-        dp = {len(s): 1}
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
-
-            if i + 1 < len(s) and (
-                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
-            ):
-                dp[i] += dp[i + 2]
-        return dp[0]
+        if s[0] == "0":
+            return 0
+    
+        first = 1
+        second = 1
+        for i in range(1, len(s)):
+            current = 0
+            if s[i] != "0":
+                current = second
+            two_digit = int(s[i - 1: i + 1])
+            if two_digit >= 10 and two_digit <= 26:
+                current += first
+            first = second
+            second = current
+        
+        return second
