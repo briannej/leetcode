a4d9cc332184ae41ce948ef595b9418220ea364f
Brian <brian.nejati@utoronto.ca>
2022-09-08 12:11:46 -0400

    #875 update

--- python/875-Koko-Eating-Bananas.py
+++ python/875-Koko-Eating-Bananas.py
@@ -1,17 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        k = 0
+        def calcHours(k):
+            sum=0
+            for pile in piles:
+                sum += math.ceil(pile/k)
+            return sum
 
-        while l <= r:
-            m = (l + r) // 2
 
-            totalTime = 0
-            for p in piles:
-                totalTime += ((p - 1) // m) + 1
-            if totalTime <= h:
-                k = m
-                r = m - 1
+        l,r =1, max(piles)
+        answer=r
+        while l<=r:
+            m= l+(r-l)//2
+            if calcHours(m) > h:
+                l=m+1
             else:
-                l = m + 1
-        return k
+                answer=m
+                r=m-1
+        return answer
