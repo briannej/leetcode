8df043ae98913da8995f459891d9da5aa062832a
Brian <brian.nejati@utoronto.ca>
2023-08-19 21:26:02 +0100

    #100 minor update

--- python/100-Same-Tree.py
+++ python/100-Same-Tree.py
@@ -5,7 +5,19 @@
 #         self.left = None
 #         self.right = None
 
+#solution 1
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        
+        if not p or not q or p.val != q.va:
+            return False       
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
+
+
+#solution 2
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if not p and not q:
