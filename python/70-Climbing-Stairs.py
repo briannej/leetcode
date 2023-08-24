8ae925b03a3736d4c2cf716df212f74710ee0b8e
Brian <brian.nejati@utoronto.ca>
2023-08-24 12:30:29 +0100

    #70 update

--- python/70-Climbing-Stairs.py
+++ python/70-Climbing-Stairs.py
@@ -1,5 +1,22 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        first, second= 0,1
+        for i in range(n):
+            first, second= second, first+second
+        return second
+    
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        hashmap={-1:0,0:1}
+        def helper(n):
+            if n not in hashmap:
+                hashmap[n]=helper(n-1)+helper(n-2)
+            return hashmap[n]
+        return helper(n)
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
         if n <= 3:
             return n
         n1, n2 = 2, 3
