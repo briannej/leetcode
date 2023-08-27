d844a90f9331b4fbff5ca62a6a604341c16984b0
Brian <brian.nejati@utoronto.ca>
2023-08-27 21:33:13 +0100

    #518 update

--- python/518-coin-change-2.py
+++ python/518-coin-change-2.py
@@ -1,3 +1,35 @@
+#bottom up 2D dp
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins))]
+
+        for row in range(len(coins)):
+            dp[row][0] = 1
+        for row in range(len(coins)):
+            for col in range(1, amount+1):
+                if coins[row] > col:
+                    dp[row][col] = dp[row-1][col]
+                else:
+                    dp[row][col] = dp[row-1][col] + dp[row][col-coins[row]]
+        return dp[-1][-1]
+
+#if we wanted to add a first row of 0s, then row-1 would corespond to coins index
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+
+        n = len(coins)
+        M = [[0 for i in range(amount+1)] for x in range(n+1)]
+        for i in range(n+1):
+            M[i][0] = 1
+        for i in range(1, n+1):
+            for j in range(1, amount+1):
+                if coins[i-1] > j:
+                    M[i][j] = M[i-1][j]
+                else:
+                    M[i][j] = M[i-1][j] + M[i][j-coins[i-1]]
+        return M[n][amount]
+
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
