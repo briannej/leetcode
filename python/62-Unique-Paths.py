078887aa7cdd677195ddae99b4f3dfba8a7a661c
Brian <brian.nejati@utoronto.ca>
2023-08-27 21:33:24 +0100

    #62 update

--- python/62-Unique-Paths.py
+++ python/62-Unique-Paths.py
@@ -1,3 +1,17 @@
+#bottom up dynamic programming
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        d = [[1] * n for _ in range(m)]
+
+        for row in range(1, m):
+            for col in range(1, n):
+                d[row][col] = d[row - 1][col] + d[row][col - 1]
+
+        return d[m - 1][n - 1]
+
+
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         row = [1] * n
