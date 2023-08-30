09829184e9f7bae2cb7952fd7f009c9eec09532a
Brian <brian.nejati@utoronto.ca>
2023-08-30 22:51:04 +0100

    #213 update

--- python/213-House-Robber-II.py
+++ python/213-House-Robber-II.py
@@ -3,10 +3,8 @@ class Solution:
         return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
 
     def helper(self, nums):
-        rob1, rob2 = 0, 0
+        first,second=0,0
+        for i in range(len(nums)):
+            first,second=second, max(first+nums[i],second)
+        return second        
 
-        for n in nums:
-            newRob = max(rob1 + n, rob2)
-            rob1 = rob2
-            rob2 = newRob
-        return rob2
