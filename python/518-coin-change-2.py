57f60d0b3c5083ec5329d6bee2042ca79e239082
Brian <brian.nejati@utoronto.ca>
2023-09-02 23:07:18 +0100

    #518 re-update

--- python/518-coin-change-2.py
+++ python/518-coin-change-2.py
@@ -1,5 +1,23 @@
 #bottom up 2D dp
 class Solution:
+  def change(self, amount: int, coins: List[int]) -> int:
+    
+    dp= [[0]* (amount+1) for _ in range(len(coins)+1)]
+    for row in range(1,len(coins)+1):
+      dp[row][0]=1
+    
+    for row in range(1,len(coins)+1):
+      for col in range(1,amount+1):
+        if col-coins[row-1] >=0:
+          dp[row][col]=dp[row-1][col]+dp[row][col-coins[row-1]]
+        else:
+          dp[row][col]=dp[row-1][col]
+        
+    return dp[-1][-1]
+
+
+#bottom up 2D dp
+class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [[0 for _ in range(amount+1)] for _ in range(len(coins))]
 
@@ -13,20 +31,7 @@ class Solution:
                     dp[row][col] = dp[row-1][col] + dp[row][col-coins[row]]
         return dp[-1][-1]
 
-#bottom up 2D dp
-class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        ROWS = len(coins)
-        dp = [[0 for i in range(amount+1)] for x in range(ROWS+1)]
-        for i in range(ROWS+1):
-            dp[i][0] = 1
-        for i in range(1, ROWS+1):
-            for j in range(1, amount+1):
-                if coins[i-1] > j:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
-        return dp[ROWS][amount]
+
 #if we wanted to add a first row of 0s, then row-1 would corespond to coins index
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
