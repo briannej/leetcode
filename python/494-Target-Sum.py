b5605c025d9d54526636f4da59a25fe226ed909e
Brian <brian.nejati@utoronto.ca>
2023-09-09 21:07:02 +0100

    #494 update

--- python/494-Target-Sum.py
+++ python/494-Target-Sum.py
@@ -20,23 +20,19 @@ class Solution:
                 ## SPACE COMPLEXITY : O(N^2) ##
 
         totalSum = sum(nums)
+        ROWS= len(nums)
         if(S not in range(-1 * totalSum, totalSum + 1) ): return 0
-        dp = [ [ 0 for j in range( totalSum*2 + 1 ) ] for i in range(len(nums))]
+        dp= [[0]*(totalSum*2+1) for _ in range(ROWS+1)]
+        dp[0][totalSum]=1
 
-        ## BASE CASE ## FIRST ROW ##
-        dp[0][totalSum + nums[0]] += 1
-        dp[0][totalSum - nums[0]] += 1
-
-        for i in range(1, len(nums)):
-            for j in range( totalSum*2 + 1 ):
-
-                if( j - nums[i] >= 0 and dp[i-1][j-nums[i]] > 0 ):          # left side
-                    dp[i][j] += dp[i-1][j-nums[i]]
-
-                if( j + nums[i] <= totalSum*2 and dp[i-1][j+nums[i]] > 0 ): # right side
-                    dp[i][j] += dp[i-1][j+nums[i]]
-
-        print(dp)
+        
+        for row in range(1,ROWS+1):
+            for col in range(totalSum*2 + 1):
+                if col - nums[row-1] >= 0:  # left side
+                    dp[row][col] += dp[row-1][col-nums[row-1]]
+                if col + nums[row-1] <= totalSum*2:  # right side
+                    dp[row][col] += dp[row-1][col+nums[row-1]]
+                
         return dp[-1][totalSum + S]
 
 
