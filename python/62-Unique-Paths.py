1b9da12aaf7e985a9d7d78014604ab961aa51f0b
Brian <brian.nejati@utoronto.ca>
2023-09-09 21:07:15 +0100

    #62 update

--- python/62-Unique-Paths.py
+++ python/62-Unique-Paths.py
@@ -1,4 +1,16 @@
-#bottom up dynamic programming
+#bottom up 2D dynamic programming
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0] *(n+1) for _ in range(m+1)]
+        dp[1][0]=1
+        for row in range(1,m+1):
+            for col in range(1,n+1):
+                dp[row][col]=dp[row-1][col]+dp[row][col-1]
+        print(dp)
+        return dp[-1][-1]
+
+
+#bottom up 2D dynamic programming
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         d = [[1] * n for _ in range(m)]
@@ -9,9 +21,33 @@ class Solution:
 
         return d[m - 1][n - 1]
 
+#bottom up 1D dynamic programming- memory optimization with one row one variable
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[0]*(n)
+        dp[0]=1
+        for _ in range(m):
+            prev=0
+            for col in range(n):
+                dp[col]+=prev
+                prev=dp[col]
+        return dp[-1]
 
+#bottom up 1D dynamic programming- memory optimization with 2 rows
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
 
+        aboveRow = [1] * n
 
+        for _ in range(m - 1):
+            currentRow = [1] * n
+            for i in range(1, n):
+                currentRow[i] = currentRow[i-1] + aboveRow[i]
+            aboveRow = currentRow
+
+        return aboveRow[-1]
+    
+    
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         row = [1] * n
