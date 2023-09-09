707ab42c41b37796d5953f76dd62f6b43774f09a
Brian <brian.nejati@utoronto.ca>
2023-09-09 21:07:44 +0100

    #97 update

--- python/97-Interleaving-Strings.py
+++ python/97-Interleaving-Strings.py
@@ -1,23 +1,52 @@
+#2D Dynamic programing- start from row 0 coloum 0
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m, n, l = len(s1), len(s2), len(s3)
-        if m + n != l:
-            return False
+        ROWS=len(s2)
+        COLS=len(s1)
+        if len(s3) != ROWS+COLS:return False
+        dp=[[False for _ in range(COLS+1)] for _ in range(ROWS+1)]
+        dp[0][0]=True                  
+        for row in range(ROWS+1):
+            for col in range(COLS+1):
+                if col>0 and s3[row+col-1]==s1[col-1]:
+                    if dp[row][col-1]:
+                        dp[row][col]= True        
+                    
+                if row>0 and s3[row+col-1]==s2[row-1]:
+                    if dp[row-1][col]:
+                        dp[row][col]= True
+                     
+        return dp[-1][-1]
+        
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        ROWS=len(s2)
+        COLS=len(s1)
+        if len(s3) != ROWS+COLS:return False
+        dp=[[False for _ in range(COLS+1)] for _ in range(ROWS+1)]
+        dp[0][0]=True
+    
+        for row in range(1,ROWS+1):
+            if s3[row-1]==s2[row-1]:
+                if dp[row-1][0]:
+                    dp[row][0]= True    
+        for col in range(1,COLS+1):
+            if s3[col-1]==s1[col-1]:
+                if dp[0][col-1]:
+                    dp[0][col]= True
 
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
-
-        for i in range(1, m + 1):
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
-
-        for j in range(1, n + 1):
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
-
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
-
-        return dp[m][n]
+                        
+        for row in range(1,ROWS+1):
+            for col in range(1,COLS+1):
+                if s3[row+col-1]==s1[col-1]:
+                    if dp[row][col-1]:
+                        dp[row][col]= True        
+                    
+                if s3[row+col-1]==s2[row-1]:
+                    if dp[row-1][col]:
+                        dp[row][col]= True
+                     
+        return dp[-1][-1]
 
 
 
