ec25ffb67f87dd950d4d72d258ed6a7d868b0ecb
Brian <brian.nejati@utoronto.ca>
2023-09-11 21:09:36 +0100

    #115 minor update

--- python/115-Distinct-Subsequences.py
+++ python/115-Distinct-Subsequences.py
@@ -7,15 +7,12 @@ class Solution:
         for i in range(m+1):
             dp[i][0] = 1
         
-        """redundant, as we have initialised dp table with full of zeros"""
-#         for i in range(1, n+1): 
-#             dp[0][i] = 0
         
         for i in range(1, m+1):
             for j in range(1, n+1):
-                dp[i][j] += dp[i-1][j] 			#if current character is skipped
+                dp[i][j] += dp[i-1][j] 			
                 if s[i-1] == t[j-1]:
-                    dp[i][j] += dp[i-1][j-1]	#if current character is used
+                    dp[i][j] += dp[i-1][j-1]
         
         return dp[-1][-1]
 
